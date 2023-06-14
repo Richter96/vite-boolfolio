a0d19109e22f17cfb3abd2c876ee1da849cf21a7
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-14 16:48:32 +0200

    add router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { } from "module";
+
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+import route from './route'
 
 createApp(App).mount('#app')
