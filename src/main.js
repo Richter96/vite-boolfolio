878929af3a742e0d935b25fe0aee8209a094e724
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-15 09:50:04 +0200

    import route

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import { } from "module";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-import route from './route'
+import { router } from './route.js'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
