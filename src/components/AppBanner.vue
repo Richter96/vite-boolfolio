b38d16b1fae12fdcbb0368ff5d7b29018c06810a
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-15 09:50:28 +0200

    create banner page

---
+++ src/components/AppBanner.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "AppBanner",
+    props: {
+        title: String
+    }
+}
+</script>
+
+<template>
+    <div class="bg-dark py-4 text-white shadow">
+        <h1 class="ms-3 ">
+            {{ title }}
+        </h1>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
