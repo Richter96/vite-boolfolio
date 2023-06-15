c7e02bbb5c18c1509841b6b72dfa71144ed8d693
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-15 11:22:34 +0200

    add section my blog

--- src/view/AppHome.vue
+++ src/view/AppHome.vue
@@ -12,6 +12,21 @@ export default {
 
 <template>
     <AppBanner title="Home Page"></AppBanner>
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold"> My Blog</h1>
+            <p class="col-md-8 fs-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel perspiciatis ducimus
+                architecto adipisci deleniti quisquam, similique nulla ea in illo enim ratione provident iste voluptatem
+                impedit
+                eligendi est tempore beatae?
+            </p>
+
+        </div>
+    </div>
+
+
+
     <AppListProject></AppListProject>
 </template>
 
