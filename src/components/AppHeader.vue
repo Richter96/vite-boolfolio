286d446f85efa28240d48c5aca56ee3a97e85fa8
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-16 16:29:59 +0200

    edit structure navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,12 +7,18 @@ export default {
 
 
 <template>
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <nav class="navbar navbar-expand-sm fill">
         <div class="container">
+            <a class="navbar-brand" href="https://github.com/Richter96">Richter96</a>
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav me-auto mt-2 mt-lg-0">
                     <li class="nav-item">
-                        <router-link class="nav-link active" :to="{ name: 'home' }" aria-current="page">Home <span
+                        <router-link class="nav-link" :to="{ name: 'home' }" aria-current="page">Home <span
                                   class="visually-hidden">(current)</span></router-link>
                     </li>
                     <li class="nav-item">
