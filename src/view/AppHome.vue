2eff44305d53f0cf3a2d6e2637d0632d5b7a1244
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-20 14:55:26 +0200

    fix carosel

--- src/view/AppHome.vue
+++ src/view/AppHome.vue
@@ -1,10 +1,64 @@
 <script>
 import AppBanner from '../components/AppBanner.vue';
 import AppListProject from '../components/AppListProject.vue';
+import Carousel3d from 'vue-carousel-3d';
+import Slide from 'vue-carousel-3d';
+import axios from 'axios';
+
 
 export default {
     name: "AppHome",
-    components: { AppBanner, AppListProject }
+    components: {
+        AppBanner,
+        AppListProject,
+        Carousel3d,
+        Slide
+    },
+    data() {
+        return {
+            baseApi: 'http://127.0.0.1:8000/',
+            projectsPath: 'api/projects',
+            loading: true,
+            projects: [],
+            error: null
+        }
+    },
+    methods: {
+        getProjects(url) {
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response);
+                    this.projects = response.data.project.data
+                    this.loading = false
+                })
+                .catch(error => {
+                    // console.log(error);
+                    this.error = error.message
+                })
+        },
+        getImagePath(path) {
+            return this.baseApi + 'storage/' + path
+        },
+        currentPage(url) {
+            // console.log(url);
+            this.getProjects(url)
+        },
+        truncateText(text) {
+            if (text.length > 100) {
+                return (text.substring(0, 100) + "...");
+            } else {
+                return text;
+            }
+        }
+    },
+    mounted() {
+        const url = this.baseApi + this.projectsPath
+        // console.log(url);
+        this.getProjects(url)
+    },
+
+
 }
 </script>
 
@@ -12,18 +66,42 @@ export default {
 
 <template>
     <AppBanner title="Home Page"></AppBanner>
-    <div class="p-5 mb-4 bg-light rounded-3">
+    <div class="p-5 mb-4 rounded-3">
         <div class="container-fluid py-5">
             <h1 class="display-5 fw-bold"> My Blog</h1>
             <p class="col-md-8 fs-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel perspiciatis ducimus
-                architecto adipisci deleniti quisquam, similique nulla ea in illo enim ratione provident iste voluptatem
-                impedit
-                eligendi est tempore beatae?
+                Benvenuti nel mio Blog! Qui ci sono alcuni dei miei progetti creati durante il corso Boolean. Un
+                percorso che ha avuto inizio dalle basi di HTML e CSS all'utilizzo avanzato di javascript e php con
+                l'utilizzo di framework come Vue e Laravel, permettendomi di entrare nel mondo dello sviluppo web.
+                <br><br> Qui sotto
+                scoprirete le mie creazioni e le sfide affrontate lungo tutto il percorso.
+
+
+
             </p>
 
         </div>
     </div>
+
+
+    <!--     <carousel-3d v-if="projects">
+        <slide v-for="(project, i) in projects" :index="i" :key="i">
+            <template slot-scope="{ index, isCurrent, leftIndex, rightIndex }">
+                <div class="card" style="width:18rem;">
+                    <img src="https://images.unsplash.com/photo-1561154464-82e9adf32764?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                         class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted ">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                        b5
+                    </div>
+                </div>
+                <img class="text-center text-uppercase" :src="getImagePath(project.image)" :alt="project.title">
+            </template>
+        </slide>
+    </carousel-3d> -->
 
 
 
--- src/view/SinglePostView.vue
+++ src/view/SinglePostView.vue
@@ -50,34 +50,39 @@ export default {
 <template>
     <AppBanner :title="project?.title"></AppBanner>
 
+
     <div class="container single_cards_container my-4">
-        <div class="card ">
+        <router-link class="btn btn-primary my-2" :to="{ name: 'home' }">
+            Indietro
+        </router-link>
+        <div class="card " v-if="project">
             <div class="card_image">
-                <img :src="getImagePath(project.image)" alt="{{project.title}}">
+                <img :src="getImagePath(project.image)" :alt="project.title">
             </div>
             <div class="card-body h-50">
-                <div class="metadata">
+                <div class="metadata ">
                     <div class="card_description">
                         <p>{{ project.description }}</p>
                     </div>
-                    <div class="author">
+                    <div class="author mb-1 ">
                         <span class=" d-inline"><strong>Author: </strong></span>
                         <span class="text_sinc d-inline">{{ project.user.name }}</span>
                     </div>
-                </div>
-                <div class="technologies">
-                    <span><strong>Technologies: </strong></span>
-                    <div class="card_technology d-inline">
-                        <span v-for="technology in project.technologies" v-if="project.type" class="pe-1">
-                            {{ technology.name }} |
-                        </span>
+                    <div class="technologies mb-1">
+                        <span><strong>Technologies: </strong></span>
+                        <div class="card_technology d-inline">
+                            <span v-for="technology in project.technologies" v-if="project.type" class="pe-1">
+                                {{ technology.name }} |
+                            </span>
+                            <span v-else>N/A</span>
+                        </div>
+                    </div>
+                    <div class="type mb-1">
+                        <span><strong>Type: </strong></span>
+                        <span class="text_sinc d-inline" v-if="project.type"> {{ project.type.name
+                        }}</span>
                         <span v-else>N/A</span>
                     </div>
-                </div>
-                <div class="type">
-                    <span class="text_sinc" v-if="project.type"><strong>Type: </strong> {{ project.type.name
-                    }}</span>
-                    <span v-else>N/A</span>
                 </div>
             </div>
         </div>
